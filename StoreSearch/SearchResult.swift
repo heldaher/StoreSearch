9a5465640e47e543e6b879c12a4a77761e3b3daa
Henri El Daher <henrieldaher@gmail.com>
2017-07-09 00:11:33 -0400

    Add a search bar and a table view

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Henri El Daher on 7/8/17.
+//  Copyright © 2017 Henri El Daher. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
